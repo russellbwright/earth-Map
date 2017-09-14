095a8d5ac73e97c1a04a7f252314dfd4452ffd6c
Russell Wright <russell.bwright@yahoo.com>
2017-09-14 01:13:59 -0500

    trying a featurecollection, to add the points

--- js/base-map/baseMapService.js
+++ js/base-map/baseMapService.js
@@ -8,8 +8,11 @@ var map
 this.eq
 
 this.initEarthQuakes = $q(function(res, rej){
-  require(["esri/dijit/BasemapToggle","esri/symbols/SimpleMarkerSymbol","esri/request","esri/map", "dojo/domReady!"], function(BasemapToggle, SimpleMarkerSymbol, esriRequest, Map) {
+  require(["esri/graphic" ,"esri/geometry/Point", "dojo/_base/array", "esri/layers/FeatureLayer", "esri/dijit/BasemapToggle", "esri/symbols/SimpleMarkerSymbol", "esri/request", "esri/map", "dojo/domReady!"], function(Graphic, Point, array, FeatureLayer, BasemapToggle, SimpleMarkerSymbol, esriRequest, Map) {
     esriConfig.defaults.io.corsEnabledServers.push("earthquake.usgs.gov");
+
+    var featureLayer;
+
      var map = new Map("map", {
           basemap: "gray",
           center: [-96.79, 32.77],
@@ -23,8 +26,49 @@ this.initEarthQuakes = $q(function(res, rej){
         basemapToggle.startup();
 
 
+
+        var featureCollection = {
+          "layerDefinition": null,
+          "featureSet": {
+            "features": [],
+            "geometryType": "esriGeometryPoint"
+          }
+        };
+        featureCollection.layerDefinition = {
+          "geometryType": "esriGeometryPoint",
+          // "objectIdField": "ObjectID",
+          "drawingInfo": {
+            "renderer": {
+              "type": "simple",
+              "symbol": {
+                "type": "esriPMS",
+                "url": "img/EqMap.png",
+                "contentType": "image/png",
+                "width": 15,
+                "height": 15
+              }
+            }
+          },
+          "fields": [{
+            "name": "ObjectID",
+            "alias": "ObjectID",
+            "type": "esriFieldTypeOID"
+          }, {
+            "name": "description",
+            "alias": "Description",
+            "type": "esriFieldTypeString"
+          }, {
+            "name": "title",
+            "alias": "Title",
+            "type": "esriFieldTypeString"
+          }]
+        };
+      
+        
+
+
         var earthquakes = esriRequest({
-          url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
+          url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson",
           handleAs: "json"
         });
         // earthquakes.then(function(response){
@@ -35,6 +79,20 @@ this.initEarthQuakes = $q(function(res, rej){
           res(data.features)
           return eq = data.features; // print the data to browser's console
           
+
+          // var feats = [];
+          // array.forEach(data.features, function(item) {
+          
+
+
+          // var geometry = new Point(item);
+          // var graphic = new Graphic(geometry);
+          // feats.push(graphic);
+          
+         
+          
+          // })
+          // featureLayer.applyEdits(feats, null, null);
         }
         
         function requestFailed(error) {
@@ -42,7 +100,15 @@ this.initEarthQuakes = $q(function(res, rej){
         }
         
         earthquakes.then(requestSucceeded, requestFailed);
+
+        
     });
+
+
+    
+
+
+
 })
 
 
